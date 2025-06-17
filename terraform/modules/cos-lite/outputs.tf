cffd433a308c66ac5adef6577f3f05c0f5394b83
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-06-17 21:24:30 +0200

    update cos-lite outputs based on cos outputs format

--- terraform/modules/cos-lite/outputs.tf
+++ terraform/modules/cos-lite/outputs.tf
@@ -1,37 +1,22 @@
-output "app_names" {
-  value = merge(
-    {
-      alertmanager = module.alertmanager.app_name,
-      catalogue    = module.catalogue.app_name,
-      grafana      = module.grafana.app_name,
-      loki         = module.loki.app_name,
-      prometheus   = module.prometheus.app_name,
-      traefik      = module.traefik.app_name,
-    }
-  )
-}
-
-output "grafana" {
-  description = "Outputs from the Grafana module"
-  value       = module.grafana
-}
-
-output "prometheus" {
-  description = "Outputs from the prometheus module"
-  value       = module.prometheus
-}
-
-output "loki" {
-  description = "Outputs from the Loki module"
-  value       = module.loki
-}
-
 output "offers" {
-  description = "URL for the various offers"
   value = {
-    grafana = juju_offer.grafana-dashboards.url,
-    prometheus = juju_offer.prometheus-receive-remote-write.url,
-    loki = juju_offer.loki-logging.url,
-    alertmanager = juju_offer.alertmanager-karma-dashboard.url
+    alertmanager_karma_dashboard = juju_offer.alertmanager-karma-dashboard
+    grafana_dashboards           = juju_offer.grafana-dashboards
+    loki_logging                 = juju_offer.loki-logging
+    prometheus                   = juju_offer.prometheus-receive-remote-write
+  }
+}
+
+# -------------- # Sub-modules -------------- #
+
+output "components" {
+  value = {
+    alertmanager  = module.alertmanager
+    catalogue     = module.catalogue
+    grafana       = module.grafana
+    prometheus    = module.prometheus,
+    loki          = module.loki
+    ssc           = module.ssc
+    traefik       = module.traefik
   }
 }
